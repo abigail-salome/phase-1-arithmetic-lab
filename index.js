6c418d4aa25c157d0f7ebe4cc730d95ff51efdcc
Salome Abigail <salomeabigail9@gmail.com>
2024-07-03 14:44:57 +0300

    created arithmetic-lab file

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+const random = Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER)
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4
+const numbers = [3,4,8,12,15,20]
+const max = Math.max(...numbers)
+ 
